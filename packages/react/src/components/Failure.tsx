11a9c99def4c2e40dd72a9ec68342c91f18a5cea
Jennie <echenimjennifer@gmail.com>
2023-10-11 00:59:52 +0400

    feat: add success and failure screens

--- packages/react/src/components/Failure.tsx
+++ packages/react/src/components/Failure.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
-function SuccessPage({ message }: { message: string }) {
+function FailurePage({ message }: { message: string }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-green-100">
+    <div className="min-h-screen flex items-center justify-center bg-red-100">
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
         <svg
-          className="w-16 h-16 mx-auto text-green-500"
+          className="w-16 h-16 mx-auto text-red-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -15,14 +13,20 @@ function SuccessPage({ message }: { message: string }) {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            d="M5 13l4 4L19 7"
+            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 10l3 3m0 0l3-3m-3 3v4"
           />
         </svg>
-        <h1 className="text-2xl font-bold mt-4">Congratulations!</h1>
+        <h1 className="text-2xl font-bold mt-4">Tough Luck!</h1>
         <p className="text-gray-600 mt-2">{message}</p>
       </div>
     </div>
   );
 }
 
-export default SuccessPage;
+export default FailurePage;
